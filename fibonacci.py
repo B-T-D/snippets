5f43deea22879b0ce66e1e45ef2235feb70f9676
Ben <bdye71@gmail.com>
2020-06-13 19:10:41 -0400

    Fib clarifications

--- fibonacci.py
+++ fibonacci.py
@@ -11,28 +11,44 @@ def fibonacci(n):
         return 1
     return fibonacci(n - 1) + fibonacci(n - 2)
 
-# Basically right, book is slightly more concise:
-
 def sm_fibonacci(n):
+    # Erroneously returns 1 for n = 0. 
     if n <= 2:
         return 1
     return fibonacci(n - 1) + fibonacci(n - 2)
 
     # This would have same asymptotic time complexity as mine. 
 
+def test():
+    """Quick test using builtin assert."""
 
-fib = [1, 1, 2, 3, 5, 8, 13]
+    # Does the function return magic-numbered first several fib numbers
+    #   correctly?
+    fib = [1, 1, 2, 3, 5, 8, 13]
 
-expecteds = [(i + 1, fib[i]) for i in range(len(fib))]
+    expecteds = [(i + 1, fib[i]) for i in range(len(fib))]
 
-for e in expecteds:
-    expected = e[1]
-    actual = fibonacci(e[0])
-    assert actual == expected, f"actual {actual}, expected {expected}"
+    for e in expecteds:
+        expected = e[1]
+        actual = fibonacci(e[0])
+        assert actual == expected, f"actual {actual}, expected {expected}"
 
-for e in expecteds:
-    expected = e[1]
-    actual = sm_fibonacci(e[0])
-    assert actual == expected, f"actual {actual}, expected {expected}"
+    for e in expecteds:
+        expected = e[1]
+        actual = sm_fibonacci(e[0])
+        assert actual == expected, f"actual {actual}, expected {expected}"
 
-print(fibonacci(10))
+    # Does the function return 0 for n = 0?
+    func = fibonacci
+    expected = 0
+    actual = func(0)
+    assert actual == expected,\
+    f"{func.__name__} fail: actual {actual}, expected {expected}"
+
+    func = sm_fibonacci
+    actual = func(0)
+    assert actual == expected,\
+    f"{func.__name__} fail: actual {actual}, expected {expected}"
+
+if __name__ == '__main__':
+    test()
